a9b1a2896283d2eabc91c554c0210c9a9bf93287
Omid Farhang <boelectronic@gmail.com>
2024-07-16 03:45:11 +0330

    fix loading error

--- qwik-angular-react-rust/qwik-micro-frontend/src/routes/index.tsx
+++ qwik-angular-react-rust/qwik-micro-frontend/src/routes/index.tsx
@@ -1,11 +1,21 @@
-import { component$ } from "@builder.io/qwik";
-
-import '../custom-elements/index.js';
+import { component$, useVisibleTask$ } from '@builder.io/qwik';
 
 export default component$(() => {
+  useVisibleTask$(() => {
+    // Import and register Angular component
+    import('../custom-elements/angular-element/main.js').then(() => {
+      console.log('Angular component loaded');
+    });
+
+    // Import and register React component
+    import('../custom-elements/react-element/static/js/main.22de2038.js').then(() => {
+      console.log('React component loaded');
+    });
+  });
+
   return (
     <div>
-      <h1>Qwik Micro-Frontend</h1>
+      <h1>Qwik Shell App</h1>
       <angular-element></angular-element>
       <react-element></react-element>
     </div>
